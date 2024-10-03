2589884faf7357f30d589dcabade9dd9f0e6179f
tucajas <tucajas@gmail.com>
2024-10-03 16:28:37 -0300

    arregle listado y agregar

--- afiliados/admin.py
+++ afiliados/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from afiliados.models import Afiliado
 
 # Register your models here.
+admin.site.register(Afiliado)
--- afiliados/models.py
+++ afiliados/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Afiliado (models.Model):
     fecha       =   models.DateField()
     nombre      =   models.CharField(max_length=20)
-    direccion   =   models.CharField(max_length=20)
+    direccion   =   models.CharField(max_length=30)
     telefono    =   models.CharField(max_length=10)
 
     def __str__(self):
